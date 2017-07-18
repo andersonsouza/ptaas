ea1acd8f6db30743deba432262aa86e8dd202a2a
Anderson Souza <anderson.kcond@gmail.com>
2017-07-18 15:15:09 -0300

    Set a few forms as inline subforms.

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.admin.decorators import register
+from django.contrib.admin.widgets import AdminTextInputWidget
+from django.db import models
 
 from projects.models import Project, Host, NetworkAdress
 
@@ -10,10 +12,20 @@ class ProjectAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'status')
 
 
+class NetworkAddressInline(admin.TabularInline):
+
+    model = NetworkAdress
+    list_display = ('protocol', 'ip_address')
+    formfield_overrides = {
+        models.TextField: {'widget': AdminTextInputWidget},
+    }
+
+
 @register(Host)
 class HostAdmin(admin.ModelAdmin):
 
     list_display = ('name', 'fqnd')
+    inlines = [NetworkAddressInline, ]
 
 
 @register(NetworkAdress)
--- scripts/admin.py
+++ scripts/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.contrib.admin.decorators import register
+from django.contrib.admin.widgets import AdminTextInputWidget
+from django.db import models
 
 from scripts.models import ScriptCategory, Script, Vulnerability, \
-    VulnerabilityCategory, Trigger
+    VulnerabilityCategory, Trigger, TriggerParameter
 
 
 @register(ScriptCategory)
@@ -27,7 +29,17 @@ class VulnerabilityAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'report_template_file')
 
 
+class TriggerParameterInline(admin.TabularInline):
+    
+    model = TriggerParameter
+    list_display = ('key', 'value')
+    formfield_overrides = {
+        models.TextField: {'widget': AdminTextInputWidget},
+    }
+
 @register(Trigger)
 class TriggerAdmin(admin.ModelAdmin):
     
     list_display = ('id', 'script', 'match_type', 'match', 'run_script', 'parameters')
+    
+    inlines = [TriggerParameterInline, ]
